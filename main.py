1f6e5ddc4b264efcf375ebaf8a04a9f17ab0b123
Ayounes91 <36197453+Ayounes91@users.noreply.github.com>
2021-03-17 13:19:31 +0100

    Main taquin projet

---
+++ main.py
@@ -0,0 +1,28 @@
+import random
+import math
+import array as arr
+
+
+
+einitial =  [[],[],[]]
+
+
+
+
+for i in range(9): # 9 est le nombre de cases
+   einitial.append((input("Entrer la valeur :")))
+
+
+print(einitial) #On affiche le tableau de base et ordonné
+
+
+random.shuffle(einitial)
+
+e = einitial
+
+print("Nous obtenons ce tableau pour commencer a jouer", e)
+
+
+
+
+
