fc75cc6c04b6c724d1d8d58e0750a53ca9cacb0e
chrislvr <cmayika17@gmail.com>
2021-03-21 22:52:09 +0100

    Add files via upload

--- taquin.py
+++ taquin.py
@@ -4,7 +4,8 @@ Created on Wed Mar 17 00:04:13 2021
 
 @author: cmayi
 """
-import random #Pour générer des chiffres aléatoirement
+import random
+from random import randint #Pour générer des chiffres aléatoirement
 def afficher(taquin):
     print(taquin)
     print(taquin[0],"|",taquin[1],"|",taquin[2])
@@ -59,30 +60,40 @@ pathConv ={
     6:"Droite",
     8:"Haut"}
     
-def generer():
-    chiffres = [1,2,3,4,5,6,7,8,"X"]
-    c1 = random.choice(chiffres) #On génère les chiffres aléatoirement parmi ceux de la liste ci-dessus
-    chiffres.remove(c1) #On retire les chiffres de la liste au fur et à mesure pour ne pas les générer à nouveau
-    c2 = random.choice(chiffres)
-    chiffres.remove(c2)
-    c3 = random.choice(chiffres)
-    chiffres.remove(c3)
-    c4 = random.choice(chiffres)
-    chiffres.remove(c4)
-    c5 = random.choice(chiffres)
-    chiffres.remove(c5)
-    c6 = random.choice(chiffres)
-    chiffres.remove(c6)
-    c7 = random.choice(chiffres)
-    chiffres.remove(c7)
-    c8 = random.choice(chiffres)
-    chiffres.remove(c8)
-    c0 = random.choice(chiffres)
-    chiffres.remove(c0)
-    return [c1,c2,c3,c4,c5,c6,c7,c8,c0] #On constitue le taquin ainsi formé
 
+def melanger():
+    chiffres=[1,2,3,4,5,6,7,8,"X"] #On cherche à générer un taquin à partir
+                                    #d'un taquin déjà résolu
+    i=0
+    deplacement = ["2","4","6","8"]
+    
+    while(i<randint(25,150)):
+        Xn = chiffres.index("X")
+        rep = random.choice(deplacement)
+        if(rep=="8"):
+            if(Xn not in [0,1,2]): #Si la case vide est sur le bord supérieur du taquin
+                       #Sinon
+                chiffres[Xn] = chiffres[Xn-3] #Monter la case vide revient à la faire se déplacer 3 cases
+                chiffres[Xn-3] = "X"                       #Vers l'arrière
+        elif(rep=="2"):
+            if(Xn not in [6,7,8]): #Si la case vide est sur le bord inférieur du taquin
+                    #Sinon
+                chiffres[Xn] = chiffres[Xn+3] #Descendre la case vide revient à la faire se déplacer 3 cases
+                chiffres[Xn+3] = "X"                       #En avant
+        elif(rep=="4"): #Pour aller à gauche
+            if(Xn not in [0,3,6]): #Si la case vide est sur le bord gauche du taquin
+               
+                chiffres[Xn] = chiffres[Xn-1] #Sinon cela revient à la reculer d'une case 
+                chiffres[Xn-1] = "X"
+        elif(rep=="6"):#Pour aller à droite
+            if(Xn not in [2,5,8]): #Si la case est sur le bord droit du taquin
+               
+                chiffres[Xn] = chiffres[Xn+1] #Sinon cela revient à l'avancer d'une case
+                chiffres[Xn+1] = "X" 
+        i = i+1
+    return chiffres
 def main():
-    taquin = generer()
+    taquin = melanger()
     i=0 #Compteur de coups joués
     chemin=[]
     while(not(victoire(taquin,[1,2,3,4,5,6,7,8,"X"],i,chemin))): #Tant qu'on ne résout pas le taquin
